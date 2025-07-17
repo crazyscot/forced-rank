bf07b06b1fbf6c594fa1c9f5b8620e644babef26
Ross Younger <crazyscot@gmail.com>
2025-07-17 21:54:40 +1200

    import fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusCircle, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
+library.add(faPlusCircle, faTrashCan)
+
 const app = createApp(App)
 app.component('SetupItem')
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
