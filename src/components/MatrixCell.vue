e8d6a90e7745f780221a89698863653e28c63a0c
Ross Younger <crazyscot@gmail.com>
2025-07-20 11:43:16 +1200

    tidy: v-if/else chain in MatrixCell

--- src/components/MatrixCell.vue
+++ src/components/MatrixCell.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import type { Response } from '../state.ts'
 
 const props = defineProps<{
   cellState: Response
 }>()
-const valid = computed(() => {
-  return props.cellState.valid
-})
-const answered = computed(() => {
-  return valid.value && props.cellState.answer !== undefined
-})
 </script>
 
 <template>
-  <td v-if="!valid" class="invalid"></td>
-  <td v-if="valid && !answered" class="valid">?</td>
-  <!-- need to write out the answer test longhand here to avoid a TS warning -->
-  <td v-if="valid && $props.cellState.answer !== undefined" class="answered">
-    {{ $props.cellState.answer + 1 }}
+  <td v-if="!props.cellState.valid" class="invalid"></td>
+  <td v-else-if="props.cellState.answer !== undefined" class="answered">
+    {{ props.cellState.answer + 1 }}
   </td>
+  <td v-else class="valid">?</td>
 </template>
